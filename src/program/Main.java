3fa6e609a70f518f5edc23954306813f7338ac00
Nathanael Reis Silva <nathancursos@gmail.com>
2023-12-26 23:08:33 -0300

    Department entity class

---
+++ src/program/Main.java
@@ -0,0 +1,10 @@
+package program;
+
+import model.entities.Department;
+
+public class Main {
+	public static void main(String[]args) {
+		Department department = new Department(23, "Books");
+		System.out.println(department);
+	}
+}
